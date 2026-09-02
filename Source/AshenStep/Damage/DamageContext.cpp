724a241a294c3e3eda9d46fd7a851265cefcbec0
DeptmerAshley <d.m.ashley02@gmail.com>
2026-09-02 13:40:30 -0700

    chore: adjust adding new files to unreal project and touch up class

---
+++ Source/AshenStep/Damage/DamageContext.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Damage/DamageContext.h"
+
+DamageContext::DamageContext()
+{
+}
+
+DamageContext::~DamageContext()
+{
+}
--- Source/AshenStep/Damage/DamageContext.h
+++ Source/AshenStep/Damage/DamageContext.h
@@ -1,35 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #pragma once
 
 #include "CoreMinimal.h"
 #include "DamageContext.generated.h"
 
+UENUM (BlueprintType)
 enum class EDamageType : uint8
 {
-	Fire,
-	Blundt,
-	Slice
+	Fire UMETA(DisplayName = "Fire"),
+	Blunt UMETA(DisplayName = "Blunt"),
+	Slice UMETA(DisplayName = "Slice")
 };
 
 USTRUCT (BlueprintType)
-struct FDamageContext
+struct ASHENSTEP_API FDamageContext
 {
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage")
 	float DamageAmount = 10.0f;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage", meta = (AllowPrivateAccess = "true"))
-	FActor DamageInstigator = 10.0f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage")
+	TObjectPtr<AActor> DamageInstigator = nullptr;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage", meta = (AllowPrivateAccess = "true"))
-	FActor DamageSource = 10.0f;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage")
+	TObjectPtr<AActor> DamageSource = nullptr;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Damage")
 	FVector DamageHitLocation = FVector::ZeroVector;
 
-	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Damage|Type", meta = (AllowPrivateAccess = "true"))
-	EDamageType DamageType;
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Damage|Type")
+	EDamageType DamageType = EDamageType::Blunt;
 
-	FDamageContext();
+	FDamageContext() = default;
 };
